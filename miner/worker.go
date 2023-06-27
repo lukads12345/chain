426f196b2692c51a2f5d0f842dbe5eca6a1af659
lukads12345 <lukads12345>
2023-06-27 17:32:12 +0800

    fix pending txs cal failed

--- miner/worker.go
+++ miner/worker.go
@@ -1025,7 +1025,12 @@ func (w *worker) commitNewWork(interrupt *int32, noempty bool, timestamp int64) 
 		// Create por challenge transaction
 		if dpos, ok := w.engine.(*dpos.Dpos); ok {
 			nonceDiff := uint64(0)
-			if w.porWork.CanLock(header.Coinbase) && len(w.challengeCh) == 0 {
+			// Fill the block with all available pending transactions.
+			tPending, err := w.eth.TxPool().Pending()
+			if err != nil {
+				log.Error("Failed to fetch pending transactions", "err", err)
+			}
+			if w.porWork.CanLock(header.Coinbase) && len(w.challengeCh) == 0 && len(tPending[header.Coinbase]) == 0 {
 				tx, seed, provider, err := dpos.TryCreateChallenge(w.chain, header, env.state)
 
 				if err == nil {
@@ -1068,6 +1073,7 @@ func (w *worker) commitNewWork(interrupt *int32, noempty bool, timestamp int64) 
 
 			}
 			if len(tTxs) > 0 {
+
 				w.eth.TxPool().AddLocals(tTxs)
 
 			}
