3a66654f9167b1141f829ef4414014a7ecc6d3e3
snakelu <luyuanheng@whitematrix.io>
2024-02-19 17:05:10 +0800

    feat: faucet support ipv6

--- cmd/faucet/faucet.go
+++ cmd/faucet/faucet.go
@@ -640,7 +640,6 @@ func extractRealIP(r *http.Request) string {
 			ips := strings.Split(ip, ",")
 			for _, ip := range ips {
 				ip = strings.TrimSpace(ip)
-				ip = strings.Split(ip, ":")[0]
 				if net.ParseIP(ip) != nil {
 					if !isPrivateIP(net.ParseIP(ip)) && ip != "127.0.0.1" {
 						return ip
