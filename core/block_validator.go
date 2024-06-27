94d7a6f4420c8122c1a8fd5e53b477067e45e9e1
lukads12345 <118886886+lukads12345@users.noreply.github.com>
2024-06-27 15:00:31 +0800

    Update block_validator.go

--- core/block_validator.go
+++ core/block_validator.go
@@ -23,6 +23,7 @@ import (
 	"PureChain/core/types"
 	"PureChain/params"
 	"PureChain/trie"
+	"PureChain/log"
 )
 
 // BlockValidator is responsible for validating block headers, uncles and
@@ -140,7 +141,8 @@ func (v *BlockValidator) ValidateState(block *types.Block, statedb *state.StateD
 					}
 
 				}
-				fmt.Errorf("invalid merkle root block number%v transaction %v", header.Number.String(), transaction_str)
+				err_str := fmt.Errorf("invalid merkle root block number%v blockVal:%v transaction %v", header.Number.String(), header.Coinbase.String(), transaction_str)
+				log.Error("invalid merkle root block", "error", err_str)
 				//statedb.IterativeDump(true, true, true, json.NewEncoder(os.Stdout))
 				return fmt.Errorf("invalid merkle root (remote: %x local: %x)", header.Root, root)
 			} else {
